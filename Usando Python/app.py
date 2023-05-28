7cb3cecadd49e2c59ee032ff7f157756fdb31c04
Pedrolucas114 <pedro114lucas@gmail.com>
2023-05-28 15:34:52 -0300

    att

---
+++ Usando Python/app.py
@@ -0,0 +1,8 @@
+idade = 18
+genero = 'M'
+
+if idade == 18 and genero == 'M':
+    print('Apresente-se para o serviço militar obrigatório')
+else:
+    print('Dispensando do serviço militar obrigatório')
+            
